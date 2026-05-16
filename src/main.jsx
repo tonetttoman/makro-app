a126e71ff8f62a72c39f38a3d6573d3fc81fad37
tonetttoman <tonett.toman@gmail.com>
2026-05-16 14:58:45 +0200

    Load restored shared styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
+import "./fixes.css";
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
