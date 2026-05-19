7aec8e335e71f9da73a7a85f3fb3354bb7c3dca4
Antal Toman <tonett.toman@gmail.com>
2026-05-19 14:17:46 +0200

    Clean up legacy CSS after dead code removal

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
-import "./fixes.css";
 import "./tailwind.css";
 
 if ("serviceWorker" in navigator) {
